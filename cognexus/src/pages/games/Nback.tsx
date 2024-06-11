b2d6d5720ba1fadf9fb58ec52026e037c930adae
Tanner Eschmann <107972255+TannerEsch@users.noreply.github.com>
2024-06-10 22:00:03 -0400

    diversified the settings; users may now select between various audiological, visual, and tactile stimuli on n-back

--- cognexus/src/pages/games/Nback.tsx
+++ cognexus/src/pages/games/Nback.tsx
@@ -3,6 +3,9 @@ import React, { useState, useEffect, useReducer } from 'react';
 interface settings {  
   nValue: number;
   prefixValue: number;
+  audio?: string[];
+  visual?: string[];
+  tactile?: string[];
 }
 
 const reducer = (state: settings, action: any) => {
@@ -13,6 +16,15 @@ const reducer = (state: settings, action: any) => {
     case 'prefixValue':
       return { ...state, prefixValue: action.value };
 
+    case 'audio':
+      return { ...state, audio: action.value };
+
+    case 'visual':
+      return { ...state, visual: action.value };
+
+    case 'tactile':
+      return { ...state, tactile: action.value };
+
     default:
       return state;
   }
@@ -22,8 +34,45 @@ const Nback: React.FC = () => {
   
   const [state, dispatch] = useReducer(reducer, {
     nValue: 1,
-    prefixValue: 1,
+    prefixValue: 2,
+    audio: [],
+    visual: [],
+    tactile: []
   });
+
+  const checkStimuli = ({type, value}) => {
+    switch (value.target.value) {
+      case "audio":
+        if (value.target.checked) {
+          dispatch({ type: 'audio', value: [...state.audio, value.target.name] });
+        } else {
+          dispatch({ type: 'audio', value: state.audio.filter((item) => item !== value.target.name) });
+        }
+        break;
+
+      case "visual":
+        if (value.target.checked) {
+          dispatch({ type: 'visual', value: [...state.visual, value.target.name] });
+        } else {
+          dispatch({ type: 'visual', value: state.visual.filter((item) => item !== value.target.name) });
+        }        
+        break;
+
+      case "tactile":
+          if (value.target.checked) {
+          dispatch({ type: 'tactile', value: [...state.tactile, value.target.name] });
+        } else {
+          dispatch({ type: 'tactile', value: state.tactile.filter((item) => item !== value.target.name) });
+        }        
+        break;
+
+      default:
+        break;
+    }
+    console.log("after", state.audio)
+    console.log("after", state.visual)
+    console.log("after", state.tactile)
+  }
 
 
   return (
@@ -73,6 +122,7 @@ const Nback: React.FC = () => {
           className="w-full border-gray-300 rounded-lg p-2 focus:ring-indigo-500 focus:border-indigo-500"
         />
       </div>
+
       <div className="mb-4">
         <label htmlFor="nValue" className="block text-sm font-medium text-gray-700 mb-1">N Value</label>
         <input
@@ -82,7 +132,149 @@ const Nback: React.FC = () => {
           onChange={(e) => dispatch({ type: 'nValue', value: e.target.value })}
           className="w-full border-gray-300 rounded-lg p-2 focus:ring-indigo-500 focus:border-indigo-500"
         />
+      </div>  
+
+      <div className='flex flex-col space-y-4 mb-2'>
+      <div>
+      <h1 className='font-bold'>Audiological Stimuli</h1>
+
+      <div className='flex space-x-2 flex-wrap'>
+
+
+      {/* AUDIOLOGICAL STIMULI INPUTS */}
+      <input
+      onChange={(e) => checkStimuli({ type: 'audio', value: e })} 
+      type="checkbox" 
+      value="audio" 
+      name="Words" 
+      id="Words"/>
+      <label htmlFor="Words" className="block text-sm font-medium text-gray-700 mb-1">Words</label>
+
+      <input 
+      onChange={(e) => checkStimuli({ type: 'audio', value: e })}
+      type="checkbox" 
+      value="audio" 
+      name="Numbers" 
+      id="Numbers" />
+      <label htmlFor="Numbers" className="block text-sm font-medium text-gray-700 mb-1">Numbers</label>
+
+      <input 
+      onChange={(e) => checkStimuli({ type: 'audio', value: e })}
+      type="checkbox" 
+      value="Letters" 
+      name="Letters" 
+      id="Letters" 
+      />
+      <label htmlFor="Numbers" className="block text-sm font-medium text-gray-700 mb-1">Letters</label>
+
+      <input 
+      onChange={(e) => checkStimuli({ type: 'audio', value: e })}
+      type="checkbox" 
+      value="audio" 
+      name="Phonemes" 
+      id="Phonemes" />
+      <label htmlFor="Phonemes" className="block text-sm font-medium text-gray-700 mb-1">Phonemes</label>
+
+      <input 
+      onChange={(e) => checkStimuli({ type: 'audio', value: e })}
+      type="checkbox" 
+      value="audio" 
+      name="Random" 
+      id="Random" />
+      <label htmlFor="Random" className="block text-sm font-medium text-gray-700 mb-1">Random</label>
+
       </div>
+
+      </div>
+      {/* AUDIOLOGICAL STIMULI INPUTS END */}
+
+      {/* VISUAL STIMULI INPUTS START */}
+      <div>
+      <h1 className='font-bold'>Visual Stimuli</h1>
+
+      <div className='flex space-x-2 flex-wrap'>
+
+      <input 
+      onChange={(e) => checkStimuli({ type: 'visual', value: e })}
+      type="checkbox" 
+      value="visual" 
+      name="Highlight" 
+      id="Highlight" 
+      />
+      <label htmlFor="Highlight" className="block text-sm font-medium text-gray-700 mb-1">Highlight</label>
+
+      <input 
+      onChange={(e) => checkStimuli({ type: 'visual', value: e })}
+      type="checkbox" 
+      value="visual" 
+      name="Gabor" 
+      id="Gabor" />
+      <label htmlFor="Gabor" className="block text-sm font-medium text-gray-700 mb-1">Gabor</label>
+
+      <input 
+      onChange={(e) => checkStimuli({ type: 'visual', value: e })}
+      type="checkbox" 
+      value="visual" 
+      name="Shapes" 
+      id="Shapes" />
+      <label htmlFor="Shapes" className="block text-sm font-medium text-gray-700 mb-1">Shapes</label>
+
+      <input 
+      onChange={(e) => checkStimuli({ type: 'visual', value: e })}
+      type="checkbox" 
+      value="visual" 
+      name="Phonemes" 
+      id="Phonemes" />
+      <label htmlFor="Phonemes" className="block text-sm font-medium text-gray-700 mb-1">Phonemes</label>
+
+      <input 
+      onChange={(e) => checkStimuli({ type: 'visual', value: e })}
+      type="checkbox" 
+      value="visual" 
+      name="Numbers" 
+      id="Numbers" />
+      <label htmlFor="Numbers" className="block text-sm font-medium text-gray-700 mb-1">Numbers</label>
+
+      <input 
+      onChange={(e) => checkStimuli({ type: 'visual', value: e })}
+      type="checkbox" 
+      value="visual" 
+      name="Letters" 
+      id="Letters" />
+      <label htmlFor="Letters" className="block text-sm font-medium text-gray-700 mb-1">Letters</label>
+
+      <input 
+      onChange={(e) => checkStimuli({ type: 'visual', value: e })}
+      type="checkbox" 
+      value="visual" 
+      name="Random" 
+      id="Random" />
+      <label htmlFor="Random" className="block text-sm font-medium text-gray-700 mb-1">Random</label>
+
+      </div>
+      </div>
+      {/* VISUAL STIMULI INPUTS END */}
+
+      {/* TACTILE STIMULI INPUTS START */}
+      <div>
+      <h1 className='font-bold'>Tactile Stimuli (mobile only)</h1>
+
+      <div className='flex space-x-2 flex-wrap'>
+
+      <input 
+      onChange={(e) => checkStimuli({ type: 'tactile', value: e })}
+      type="checkbox" 
+      value="tactile" 
+      name="Vibrations" 
+      id="Vibrations" />
+      <label htmlFor="Vibrations" className="block text-sm font-medium text-gray-700 mb-1">Vibrations</label>
+
+      </div>
+
+      </div>
+      {/* TACTILE STIMULI INPUTS END */}
+      </div>
+
 
       <button
         // onClick={handleSave}
