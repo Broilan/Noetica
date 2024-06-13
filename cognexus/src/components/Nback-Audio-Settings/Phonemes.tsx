9721d75199d10255f2a60ce8a73ecb49042f2559
Tanner Eschmann <107972255+TannerEsch@users.noreply.github.com>
2024-06-13 02:35:38 -0400

    moved n-back's audiological stimuli components into a new file

---
+++ cognexus/src/components/Nback-Audio-Settings/Phonemes.tsx
@@ -0,0 +1,4 @@
+const phonemes = [
+    "b", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", 
+    "w", "z", "ch", "sh", "th", "ng", "ee", "ay", "oo", "ow", "oy", "ar", "er", "or"
+]
---
+++ cognexus/src/components/Nback-Audio-Settings/Words.tsx
@@ -0,0 +1,6 @@
+const words = [
+    "apple", "bicycle", "crimson", "dog", "elephant", "forest", "grape", "house", 
+    "island", "jacket", "kangaroo", "lemon", "mountain", "notebook", "ocean", 
+    "pencil", "quartz", "river", "sunshine", "tiger", "umbrella", "violin", 
+    "whisper", "xylophone", "yellow", "zebra"
+]
