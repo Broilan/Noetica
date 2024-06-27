0f4846141a36de1a8490eae49ff1896805f29d04
Tanner Eschmann <107972255+TannerEsch@users.noreply.github.com>
2024-06-27 08:23:30 -0400

    guidelines work

--- cognexus/src/games/Nback/components/Guidelines.tsx
+++ cognexus/src/games/Nback/components/Guidelines.tsx
@@ -1,5 +1,5 @@
-// src/components/Guidelines.tsx
 import React from 'react';
+import Tooltip from '../../../components/Tooltip';
 
 interface GuidelinesProps {
   isOpen: boolean;
@@ -9,19 +9,57 @@ interface GuidelinesProps {
 const Guidelines: React.FC<GuidelinesProps> = ({ isOpen, onClose }) => {
   return (
     <div className={`fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center ${isOpen ? 'block' : 'hidden'}`}>
-      <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
+      <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full max-h-screen overflow-hidden">
         <h1 className="text-2xl font-semibold mb-4">Guidelines</h1>
         
-        <div className="mb-6">
-            <h3>Intro</h3>
-            <p className="mb-2">The N-back task is a continuous performance task that was designed by Wayne Kirchner in 1958 for the purpose of assessing working memory maintenance, updating, and monitoring.</p>
-            <h2>How to Play</h2>
-            <p>Upon beginning the game, you'll see a 3 by 3 matrix, and new stimuli will arise in each segment of it every round according to your settings. Each round, it's your job to determine whether you've seen any of the current stimuli <em>n</em> rounds ago. If you have, you'll select which one(s) using the the buttons below the board. You may also use the a, s, d, f, 1, 2, 3, and 4 if you're using a PC.  </p>
-          <p className="mb-2">2. Your task is to remember the stimulus from N steps back.</p>
-          <p className="mb-2">3. If the current stimulus matches the one from N steps back, press the corresponding button.</p>
-          <p className="mb-2">4. The game includes various types of stimuli: position, audio, shape, and color.</p>
-          <p className="mb-2">5. As you progress, the N-back level may increase, making the game more challenging.</p>
-          <p className="mb-2">6. Try to respond as quickly and accurately as possible.</p>
+        <div className="mb-6 overflow-auto h-96 custom-scrollbar">
+        <hr className='mb-2 bg-[#ccc] h-[2px]'/>
+            <h2 className="text-xl font-semibold mb-2">Intro</h2>
+            <p className="mb-4">The N-back task is a continuous performance task that was designed by Wayne Kirchner in 1958 to assess working memory maintenance, updating, and monitoring.</p>
+            <h2 className="text-xl font-semibold mb-2">How to Play</h2>
+            <p className='mb-4'>Upon beginning the game, you'll see a 3 by 3 matrix, and new stimuli will arise in each segment of it every round according to your settings. Each round, it's your job to determine whether you've seen any of the current stimuli <em>n</em> rounds ago. If you have, you'll select which one(s) using the the buttons below the board. You may also use the <kbd>a</kbd>, <kbd>s</kbd>, <kbd>d</kbd>, <kbd>f</kbd>, <kbd>1</kbd>, <kbd>2</kbd>, <kbd>3</kbd>, and <kbd>4</kbd> keys if you're using a keyboard.  </p>
+            <h1 className="text-2xl font-semibold mb-2">Settings</h1>
+            <hr className='mb-2 bg-[#ccc] h-[2px]'/>
+            <p className='mb-4'>Below are descriptions of the various settings you can manipulate to customize your game.</p>
+            <h2 className='text-xl font-semibold mb-2 underline'>Audiological</h2>
+            <ol>
+
+                <Tooltip text='Smallest units of meaning in a language'>
+                <li className='mb-2'><a href="https://en.wikipedia.org/wiki/Phoneme">
+                <strong className='text-blue-600'>Phonemes:</strong> English phonemes 
+                </a>
+                </li>
+                </Tooltip>
+
+                <li className='mb-2'><strong>Words:</strong> Polysyllabic English words </li>
+                <li className='mb-2'><strong>Letters:</strong> Alphabetic characters</li>
+                <Tooltip text='These are the numbers we use globally'>
+                 <li className='mb-2'><strong>Numbers:</strong> Hindu-Arabic numerals</li> 
+                </Tooltip>
+            </ol>
+            <h2 className='text-xl font-semibold mb-2 underline'>Imagistic</h2>
+            <ol>
+              <li className='mb-2'><strong>Shapes:</strong> Various geometric forms</li>
+              <li className='mb-2'>
+                <a  href="https://en.wikipedia.org/wiki/Gabor_filter">
+                  <strong className="text-blue-600">Gabor Patches:</strong> Visual patterns used in neuroscience
+                </a>
+                </li>
+              <li className='mb-2'><strong>Letters:</strong> Alphabetic characters</li>
+
+              <Tooltip text="Tetris blocks">
+              <li className='mb-2'>
+              <a href="https://en.wikipedia.org/wiki/Pentomino">
+                <strong className='text-blue-600'>Pentominoes:</strong> A polygon comprised of 5 tiles
+              </a>
+              </li>
+              </Tooltip>
+
+            </ol>
+            <h2 className='text-xl font-semibold mb-2 underline'>Positional</h2>
+            <p className='mb-4'>When this setting's on, if you're using imagistic stimuli, chromatic stimuli, or both, you'll simply select the "position" button if you've seen either of the formentioned stimuli <em>n</em> turns ago.</p>
+            <h2 className='text-xl font-semibold mb-2 underline'>Chromatic</h2>
+            <p className='mb-4'> When this setting's turned on alongside imagistic mode, the images will take on various colors, and you'll need to determine whether you've seen that color <em>n</em> turns ago. Without imagistic stimuli turned on, the squares themselves will be highlighted. </p>
         </div>
 
         <button onClick={onClose} className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors duration-200">
