a6766f240e5e2ebbda0643ff2f715048f36e3076
Tanner Eschmann <107972255+TannerEsch@users.noreply.github.com>
2024-06-27 04:20:30 -0400

    more board styling

--- cognexus/src/games/Nback/components/GameBoard.tsx
+++ cognexus/src/games/Nback/components/GameBoard.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ControlPanel from './ControlPanel';
 import { FaGear } from "react-icons/fa6";
 import { FaBook } from "react-icons/fa";
+import { IoMdStopwatch } from "react-icons/io";
+import { LuGauge } from "react-icons/lu";
 
 interface GameBoardProps {
   stimuli: {
@@ -16,62 +18,75 @@ interface GameBoardProps {
 const GameBoard: React.FC<GameBoardProps> = ({ stimuli, nBackLevel }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isGameStarted, setIsGameStarted] = useState(false);
+  const [time, setTime] = useState(0);
+  const [score, setScore] = useState(0);
 
-  const renderImageContent = (subType: string) => {
-    switch (subType) {
-      case 'shape':
-        return <div className="w-full h-full bg-blue-500" />;
-      case 'gabor':
-        return <div className="w-full h-full bg-gradient-to-br from-black to-white" style={{ background: 'repeating-linear-gradient(45deg, black, black 5px, white 5px, white 10px)' }} />;
-      case 'letter':
-        return <span className="text-2xl font-bold">A</span>;
-      case 'number':
-        return <span className="text-2xl font-bold">1</span>;
-      default:
-        return null;
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (isGameStarted) {
+      timer = setInterval(() => {
+        setTime(prevTime => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(timer);
     }
+    return () => clearInterval(timer);
+  }, [isGameStarted]);
+
+  const handleStop = () => {
+    setIsGameStarted(false);
   };
 
+  const handleStart = () => {
+    setIsGameStarted(true);
+    setTime(0);
+  };
+
+
   return (
-    <div className="relative p-6 bg-white shadow-xl border-gray-500 border-2 rounded-lg max-w-lg mx-auto">
-      <div className='flex flex-row'>
-        <i className=" text-gray-500 hover:text-gray-700"><FaBook /></i>
+    <div className="relative p-6 bg-white shadow-xl border-gray-500 border-2 rounded-lg max-w-lg mx-auto" style={{ width: '400px', height: '600px' }}>
+      <div className='flex flex-row justify-between mb-4'>
+        <i className="text-gray-500 hover:text-gray-700 text-2xl"><FaBook /></i>
         <button
           onClick={() => setIsModalOpen(true)}
-          className=" text-gray-500 hover:text-gray-700"
+          className="text-gray-500 hover:text-gray-700 text-2xl"
         >
           <i><FaGear /></i>
         </button>
       </div>
 
-      <div className="grid grid-cols-3 gap-4">
+      <div className="flex text-xl font-bold justify-between items-center mb-4">
+        <div className="text-center"><IoMdStopwatch />{time}s</div>
+        <div className="text-center"><LuGauge />{score}:1</div>
+      </div>
+
+      <div className="grid grid-cols-3 gap-4" style={{ height: '60%' }}>
         {Array.from({ length: 9 }).map((_, idx) => (
-          <div key={idx} className="w-24 h-24 bg-gray-100 border border-gray-300 flex items-center justify-center rounded-lg">
+          <div key={idx} className="w-full h-full bg-gray-100 border border-gray-300 flex items-center justify-center rounded-lg">
             {stimuli.color && <div className="w-full h-full bg-red-500" />}
-            {stimuli.image && renderImageContent(stimuli.image)}
             {stimuli.position && <span className="text-lg font-semibold">{idx + 1}</span>}
           </div>
         ))}
       </div>
 
       {isGameStarted ? (
-        <div className="mt-4 flex justify-between">
+        <div className="mt-2 font-bold text-sm" style={{ height: '30%' }}>
           <button
-            onClick={() => setIsGameStarted(false)}
-            className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-700"
+            onClick={handleStop}
+            className="p-2 bg-red-500 text-white rounded-md hover:bg-red-700"
           >
             Stop
           </button>
-          <div className="flex space-x-4">
-            <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700">1: Position</button>
-            <button className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-700">2: Audio</button>
-            <button className="px-4 py-2 bg-yellow-500 text-white rounded-md hover:bg-yellow-700">3:Shape</button>
-            <button className="px-4 py-2 bg-purple-500 text-white rounded-md hover:bg-purple-700">4: Color</button>
+          <div className="flex flex-row space-x-2">
+            <button className="p-3 bg-blue-500 text-white rounded-md hover:bg-blue-700">1: Position</button>
+            <button className="p-3 bg-green-500 text-white rounded-md hover:bg-green-700">2: Audio</button>
+            <button className="p-3 bg-yellow-500 text-white rounded-md hover:bg-yellow-700">3: Shape</button>
+            <button className="p-3 bg-purple-500 text-white rounded-md hover:bg-purple-700">4: Color</button>
           </div>
         </div>
       ) : (
         <button
-          onClick={() => setIsGameStarted(true)}
+          onClick={handleStart}
           className="mt-4 px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-700"
         >
           Start
